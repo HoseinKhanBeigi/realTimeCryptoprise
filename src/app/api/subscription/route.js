ec800da356ea192417965de3eda47569ece40ba0
hossein <hossein@hosseins-MacBook-Pro.local>
2025-02-09 09:44:15 +0100

    fix

--- src/app/api/subscription/route.js
+++ src/app/api/subscription/route.js
@@ -1,4 +1,7 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   // ✅ Import Nodemailer dynamically for Gatsby functions
   const nodemailer = require("nodemailer");
 
